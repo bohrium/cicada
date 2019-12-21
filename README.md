f4ccc2bcac5c57c2f3215a12cb4f4361fccaa0b9
Samuel <samuel@umich.edu>
2019-12-20 22:54:26 -0500

    first toy model

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# cicada
+[//]: # (author: samtenka)
+[//]: # (change: 2019-12-19)
+[//]: # (create: 2019-12-19)
+[//]: # (descrp: )
+[//]: # (to use: Open and read this document in a web browser.)
+
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
+# Cicada: a Time-Dependent Bug Predictor
+
+We model the bug pattern in a parameterized code base over time.  We imagine a
+set of source files that is indexed by a fixed set but whose elements' contents
+change over time.  Several categorical attributes determine the interaction of
+modules and hence of code base behavior.  We seek to predict from data which 
+files will be buggy given some a configuration of attributes. 
+
+Many complications come to mind.  For example, one could statically analyze the
+source ranging from shallow (dependency graphs) to deep (formal verification).
+And one could learn the reliability of individual developers or of individual
+code base versions.  But we will take a much simpler approach: we will just do
+logistic regression; if that doesn't suffice, we can elaborate our model.
+
+## 0. Model
+
+We fit a function
---
+++ toy_data.py
@@ -0,0 +1,47 @@
+''' author: samtenka
+    change: 2019-12-20
+    create: 2019-12-20
+    descrp: fully connected classifier on MNIST 
+    to use: Run `python fully_connected.py`.
+'''
+
+import tensorflow as tf
+import numpy as np
+
+###############################################################################
+#                         0. LIST PROGRAM PARAMETERS                          #
+###############################################################################
+
+# 0.0. (Hyper)Parameters of Stochastic Gradient Descent:
+
+nb_feats = 12
+time_min = 0.0 
+time_max = 5.0
+nb_texts = 100
+nb_tbins = 5
+
+def sigmoid(arr):
+    return 1.0 / (1.0+np.exp(-arr))
+
+w_m = np.random.normal(size=(nb_feats, nb_texts)) / (time_max-time_min)
+w_b = np.random.normal(size=(nb_feats, nb_texts))
+def generate(nb_samples, threshold=0.999):
+    ''' basic generative model: t doesn't matter '''
+    x = np.random.choice(2, size=(nb_samples, nb_feats)) 
+    t = np.random.uniform(size=(nb_samples, 1)) * (time_max-time_min) + time_min
+    p = sigmoid(np.matmul(t*x, w_m) + np.matmul(x, w_b))
+    #y = np.random.binomial(n=1, p=p, size=(nb_samples, nb_texts))
+    y = (threshold<p).astype(np.int32)
+    return (x, t, y)
+
+def featurize(x, t, y):
+    ''' the number of features is one less than the number of bins '''
+    time_bins = np.arange(time_min, time_max, (time_max-time_min)/nb_tbins)
+    ts = np.repeat(t, repeats=nb_tbins-1, axis=1)
+    t_binned = (ts < np.expand_dims(time_bins[1:], axis=0))
+    return x.astype(np.float32), t_binned.astype(np.float32), y
+
+N = 10000
+X, T, Y = generate(N)
+print('~ %.3f bugs' % (np.sum(Y)/(N*nb_texts)))
+
